d608319b8f0208fc8ce02dc5ff24f6bef7bb82f1
Alfonso De Gregorio <adg@crypto.lo.gy>
2011-08-10 04:04:19 +0200

    sberla.hrl defines

---
+++ include/sberla.hrl
@@ -0,0 +1,5 @@
+-define(CLIENT, "sberla"). %% client identity
+-define(APPVER, "0.1").    %% client version
+-define(PVER, "3.0").      %% protocol version supported by sberla
+
+-define(SAFEBROWSING_PATH, "/safebrowsing/api/lookup"). % SB Lookup API Path
